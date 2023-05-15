7e91f03a692ee9b379829eb2339ed251f8f0dcdd
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-15 08:21:54 +0000

    lock

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Creates a function for the open boxes 
+"""
+
+
+def canUnlockAll(containrs):
+    """
+    Checks if all boxes are open
+    """
+    if not containrs or type(containrs) is not list:
+        return False
+
+    opened_box = [0]
+    for n in opened_box:
+        for key in containrs[n]:
+            if key not in opened_box and key < len(containrs):
+                opened_box.append(key)
+    if len(opened_box) == len(containrs):
+        return True
+    return False
